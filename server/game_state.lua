7241decbbb159840465527578c03fab8e509fec1
Peter Kish <pkish023@gmail.com>
2017-09-09 11:16:55 +0200

    Added fade in/out when respawning

--- server/game_state.lua
+++ server/game_state.lua
@@ -1,10 +1,15 @@
 local ending_state_time = 5000;
 local round_number = 0
 
+local function fade_camera_to_black(player)
+    fadeCamera(player, false, 1, 0, 0, 0)
+end
+
 local function trigger_round_end(winning_team_name)
     for i,player in ipairs(getElementsByType("player")) do
         round_number = round_number + 1
         triggerEvent("onRoundEnd", player, winning_team_name)
+        fadeCamera(player, true, 1, 0, 0, 0)
     end
 end
 
@@ -15,11 +20,15 @@ local function player_wasted_handler()
     for i,player in ipairs(getElementsByType("player")) do
         if alive_ct_count == 0 then
             triggerEvent("onRoundEnding", player, "t")
-            setTimer(trigger_round_end, ending_state_time, 1, "t")
         elseif alive_t_count == 0 then
             triggerEvent("onRoundEnding", player, "ct")
-            setTimer(trigger_round_end, ending_state_time, 1, "ct")
         end
+        setTimer(fade_camera_to_black, ending_state_time - 1000, 1, player)
+    end
+    if alive_ct_count == 0 then
+        setTimer(trigger_round_end, ending_state_time, 1, "t")
+    elseif alive_t_count == 0 then
+        setTimer(trigger_round_end, ending_state_time, 1, "ct")
     end
 end
 
