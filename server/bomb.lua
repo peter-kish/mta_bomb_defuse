e2544f94a6085c8e087ef55d729e42c570e56f60
Peter Kish <pkish023@gmail.com>
2017-09-11 22:18:20 +0200

    Basic bomb planting functionality

---
+++ server/bomb.lua
@@ -0,0 +1,159 @@
+local bomb_carrier = nil
+
+local bomb_dropped_col = nil
+local bomb_dropped_obj = nil
+
+local bomb_planted_col = nil
+local bomb_planted_obj = nil
+
+local bomb_site_a = {["x1"] = 2036.966796875, ["y1"] = -1682.4130859375, ["x2"] = 2046.388671875, ["y2"] = -1662.7763671875, ["z"] = 13.546875}
+
+local bomb_timer = nil
+local bomb_time = 10000
+local bomb_radius = 10
+local bomb_strength = 10
+local bomb_explosion_delay = 250
+
+local radar_area_bomb_site_a = createRadarArea(math.min(bomb_site_a.x1, bomb_site_a.x2),
+    math.min(bomb_site_a.y1, bomb_site_a.y2),
+    math.abs(bomb_site_a.x1 - bomb_site_a.x2),
+    math.abs(bomb_site_a.y1 - bomb_site_a.y2),
+    0, 255, 0, 128)
+
+local function round_start_handler()
+    local terrorist = getPlayersInTeam(getTeamFromName(team_t_name))
+    if #terrorist > 0 then
+        local random_player_index = math.random(1, #terrorist)
+        bomb_carrier = terrorist[random_player_index]
+        outputChatBox("You have the bomb!", bomb_carrier, 255, 0 ,0)
+    end
+end
+
+local function create_dropped_bomb(x, y, z)
+    bomb_dropped_obj = createObject(2891, x, y, z)
+    bomb_dropped_col = createColSphere(x, y, z, 2)
+    attachElements(bomb_dropped_obj, bomb_dropped_col)
+end
+
+local function create_planted_bomb(x, y, z)
+    bomb_planted_obj = createObject(2891, x, y, z)
+    bomb_planted_col = createColSphere(x, y, z, 3)
+    attachElements(bomb_planted_obj, bomb_planted_col)
+end
+
+local function random_explosion(x, y, z, radius)
+    local angle = math.random(0, 359)
+    local delta_x = math.cos(angle) * radius
+    local delta_y = math.sin(angle) * radius
+    createExplosion(x + delta_x, y + delta_y, z, 0)
+end
+
+local function multi_explosion(x, y, z, n, radius)
+    createExplosion(x, y, z, 0)
+    if n > 1 then
+        setTimer(function() random_explosion(x, y, z, radius) end, bomb_explosion_delay, n - 1)
+    end
+end
+
+local function bomb_explode()
+    local x, y, z = getElementPosition(bomb_planted_obj)
+
+    if bomb_planted_obj then
+        destroyElement(bomb_planted_obj)
+        bomb_planted_obj = nil
+    end
+    if bomb_planted_col then
+        destroyElement(bomb_planted_col)
+        bomb_planted_col = nil
+    end
+    
+    multi_explosion(x, y, z, bomb_strength, bomb_radius)
+    for i,p in ipairs(getElementsByType("player")) do
+        triggerEvent("onBombExploded", p)
+    end
+    
+    bomb_timer = nil
+end
+
+local function plant_bomb(player)
+    if player == bomb_carrier then
+        local player_x, player_y, player_z = getElementPosition(player)
+        if isInsideRadarArea(radar_area_bomb_site_a, player_x, player_y) then
+            create_planted_bomb(player_x, player_y, player_z)
+            outputChatBox("The bomb has been planted!", getRootElement(), 255, 0, 0)
+            bomb_carrier = nil
+            
+            bomb_timer = setTimer(bomb_explode, bomb_time, 1)
+            
+            for i,p in ipairs(getElementsByType("player")) do
+                triggerEvent("onBombPlanted", p)
+            end
+        else
+            outputChatBox("You have to be on a bomb site to plant the bomb!", player)
+        end
+    else
+        outputChatBox("You don't have the bomb!", player)
+    end
+end
+
+local function player_wasted_handler()
+    if source == bomb_carrier then
+        local terrorist = getPlayersInTeam(getTeamFromName(team_t_name))
+        for i,player in ipairs(terrorist) do
+            outputChatBox(getPlayerName(bomb_carrier) .. " has dropped the bomb!", player, 255, 0 ,0)
+        end
+        bomb_carrier = nil
+
+        local player_x, player_y, player_z = getElementPosition(source)
+        create_dropped_bomb(player_x, player_y, player_z)
+    end
+end
+
+local function col_shape_handler(player, dimension)
+    if (source == bomb_dropped_col) and (getElementType(player) == "player") then
+        if (getTeamName(getPlayerTeam(player)) == team_t_name) and (not isPedDead(player)) then
+            bomb_carrier = player
+            for i, terrorist in ipairs(getPlayersInTeam(getTeamFromName(team_t_name))) do
+                outputChatBox(getPlayerName(player) .. " picked up the bomb", terrorist, 255, 0 ,0)
+            end
+            destroyElement(bomb_dropped_obj)
+            destroyElement(bomb_dropped_col)
+            bomb_dropped_obj = nil
+            bomb_dropped_col = nil
+        end
+    end
+end
+
+local function round_end_handler(winning_team_name)
+    -- Clean up the bomb elements
+    if bomb_dropped_obj then
+        destroyElement(bomb_dropped_obj)
+        bomb_dropped_obj = nil
+    end
+    if bomb_dropped_col then
+        destroyElement(bomb_dropped_col)
+        bomb_dropped_col = nil
+    end
+    if bomb_planted_obj then
+        destroyElement(bomb_planted_obj)
+        bomb_planted_obj = nil
+    end
+    if bomb_planted_col then
+        destroyElement(bomb_planted_col)
+        bomb_planted_col = nil
+    end
+    -- Kill the timer, just in case
+    if bomb_timer then
+        killTimer(bomb_timer)
+    end
+end
+
+local function join_handler()
+    bindKey(source, "F5", "down", function (key_presser) plant_bomb(key_presser) end)
+end
+
+addEventHandler("onRoundStart", getRootElement(), round_start_handler )
+addEventHandler("onPlayerWasted", getRootElement(), player_wasted_handler)
+addEventHandler("onColShapeHit", getRootElement(), col_shape_handler)
+addEventHandler("onRoundEnd", getRootElement(), round_end_handler)
+addEventHandler("onPlayerJoin", getRootElement(), join_handler)
--- server/game_state.lua
+++ server/game_state.lua
@@ -1,5 +1,6 @@
 local ending_state_time = 5000;
 local round_number = 0
+local bomb_planted = false
 
 local function fade_camera_to_black(player)
     fadeCamera(player, false, 1, 0, 0, 0)
@@ -20,16 +21,36 @@ local function player_wasted_handler()
     for i,player in ipairs(getElementsByType("player")) do
         if alive_ct_count == 0 then
             triggerEvent("onRoundEnding", player, team_t_name)
-        elseif alive_t_count == 0 then
+            setTimer(fade_camera_to_black, ending_state_time - 1000, 1, player)
+        elseif (alive_t_count == 0) and (bomb_planted == false) then
             triggerEvent("onRoundEnding", player, team_ct_name)
+            setTimer(fade_camera_to_black, ending_state_time - 1000, 1, player)
         end
-        setTimer(fade_camera_to_black, ending_state_time - 1000, 1, player)
     end
     if alive_ct_count == 0 then
         setTimer(trigger_round_end, ending_state_time, 1, team_t_name)
-    elseif alive_t_count == 0 then
+    elseif (alive_t_count == 0) and (bomb_planted == false) then
         setTimer(trigger_round_end, ending_state_time, 1, team_ct_name)
     end
 end
 
+local function bomb_exploded_handler()
+    for i,player in ipairs(getElementsByType("player")) do
+        triggerEvent("onRoundEnding", player, team_t_name)
+        setTimer(fade_camera_to_black, ending_state_time - 1000, 1, player)
+    end
+    setTimer(trigger_round_end, ending_state_time, 1, team_t_name)
+end
+
+local function bomb_planted_handler()
+    bomb_planted = true
+end
+
+local function round_start_handler()
+    bomb_planted = false
+end
+
 addEventHandler("onPlayerWasted", getRootElement(), player_wasted_handler)
+addEventHandler("onBombExploded", getRootElement(), bomb_exploded_handler)
+addEventHandler("onBombPlanted", getRootElement(), bomb_planted_handler)
+addEventHandler("onRoundStart", getRootElement(), round_start_handler)
