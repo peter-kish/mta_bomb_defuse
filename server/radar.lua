f1b914073cedb6f2f9a8c6506ecddef4563cf759
Peter Kish <pkish023@gmail.com>
2017-10-08 11:13:00 +0200

    Fixed the player blips not showing up sometimes

--- server/radar.lua
+++ server/radar.lua
@@ -1,6 +1,10 @@
 local bomb_blip = nil
 
 local function setElementVisibleToTeam(element, team)
+    if not isElement(element) then
+        return false
+    end
+
     setElementVisibleTo(element, getRootElement(), false)
     for i,player in ipairs(getPlayersInTeam(team)) do
         setElementVisibleTo(element, player, true)
@@ -8,6 +12,10 @@ local function setElementVisibleToTeam(element, team)
 end
 
 local function get_player_blip(player)
+    if not isElement(player) then
+        return false
+    end
+
     for j,blip in ipairs(getAttachedElements(player)) do
         if getElementType(blip) == "blip" then
             return blip
@@ -21,43 +29,36 @@ local function player_wasted_handler()
     setElementVisibleTo(blip, getRootElement(), false)
 end
 
-local function round_start_handler()
-    -- Destroy the bomb radar blip
-    if bomb_blip then
-        destroyElement(bomb_blip)
-        bomb_blip = nil
+local function player_spawn_handler()
+    local blip = get_player_blip(source)
+    -- Creat the blip if needed
+    if not isElement(blip) then
+        blip = createBlipAttachedTo(source)
     end
-    -- Set the team radar blips visibility
-    for i,player in ipairs(getElementsByType("player")) do
-        local blip = get_player_blip(player)
-        if blip then
-            setElementVisibleToTeam(blip, getPlayerTeam(player))
-            local player_team_name = getTeamName(getPlayerTeam(player))
-            if player_team_name == team_t_name then
-                setBlipColor(blip, 255, 0, 0, 255)
-            elseif player_team_name == team_ct_name then
-                setBlipColor(blip, 0, 0, 255, 255)
-            end
-        end
+    -- Show it to teammates only
+    setElementVisibleToTeam(blip, getPlayerTeam(source))
+    
+    -- Set the blip color
+    local player_team_name = getTeamName(getPlayerTeam(source))
+    if player_team_name == team_t_name then
+        setBlipColor(blip, 255, 0, 0, 255)
+    elseif player_team_name == team_ct_name then
+        setBlipColor(blip, 0, 0, 255, 255)
     end
 end
 
 local function round_ending_handler()
     -- Destroy the bomb radar blip
-    if bomb_blip then
+    if isElement(bomb_blip) then
         destroyElement(bomb_blip)
         bomb_blip = nil
     end
 end
 
-local function player_join_handler()
-    local blip = createBlipAttachedTo(source)
-    setElementVisibleTo(blip, getRootElement(), false)
-end
-
 local function player_quit_handler()
-    for i,element in ipairs(getAttachedElements(source)) do
-        destroyElement(element)
+    local blip = get_player_blip(source)
+    if isElement(blip) then
+        destroyElement(blip)
     end
 end
 
@@ -74,16 +75,15 @@ local function bomb_planted_handler(bomber, bomb_time, bomb_planted_obj)
 end
 
 local function bomb_picked_up_handler(player)
-    if bomb_blip then
+    if isElement(bomb_blip) then
         destroyElement(bomb_blip)
         bomb_blip = nil
     end
 end
 
 addEventHandler("onPlayerWasted", getRootElement(), player_wasted_handler)
-addEventHandler("onRoundStart", mtacs_element, round_start_handler)
 addEventHandler("onRoundEnding", mtacs_element, round_ending_handler)
-addEventHandler("onPlayerJoin", getRootElement(), player_join_handler)
+addEventHandler("onPlayerSpawn", getRootElement(), player_spawn_handler)
 addEventHandler("onPlayerQuit", getRootElement(), player_quit_handler)
 addEventHandler("onBombDropped", getRootElement(), bomb_dropped_handler)
 addEventHandler("onBombPlanted", getRootElement(), bomb_planted_handler)
